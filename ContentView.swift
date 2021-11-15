ef07b53ee4d70722e8c2a9d9268b0e9689d5014d
vincent santos <vincentsantos@outlook.fr>
2021-11-15 16:56:57 +0100

    start

--- ContentView.swift
+++ ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var email = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Image("Logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250, height: 250)
+                    .cornerRadius(150)
+                
+                VStack {
+                    TextField("Email Adresse", text: $email)
+                        .padding()
+                    SecureField("Email Adresse", text: $email)
+                        .padding()
+                }
+                .padding()
+            }
+            .navigationTitle("Sign in")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
